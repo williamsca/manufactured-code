b0244f161389cb14d895732581e9982304be7c7b
Colin Williams <williams.colinandrew@gmail.com>
2026-03-30 16:14:16 -0400

    nfip estimation with the treated indicator

--- program/estimate-nfips.R
+++ program/estimate-nfips.R
@@ -14,6 +14,8 @@ dt[, damage_contents_pvalue := contents_damage_tot / contents_value_tot]
 
 dt[, net_building_pmts_pdamage := net_building_pmt_tot / building_damage_tot]
 
+dt[, treated_mh := (mh == 1) & treated == TRUE]
+
 dt_claims <- dt[claims_n > 0 & building_damage_tot > 0]
 
 # estimate ----
@@ -21,22 +23,23 @@ dt_claims <- dt[claims_n > 0 & building_damage_tot > 0]
 # main specification: event study with treated × year_constr effects,
 # controlling for type x year_loss x county FEs
 v_pclaim <- c(
-    "building_value_pclaim", "building_damage_pclaim",
+    "building_damage_pclaim", # "building_value_pclaim", 
     "net_building_pmt_pclaim",
     "contents_damage_pclaim", "net_contents_pmt_pclaim",
-    "contents_value_pclaim",
-    "building_covg_pclaim", "contents_covg_pclaim",
-    "damage_building_pvalue", "damage_contents_pvalue",
-    "net_building_pmts_pdamage")
+    "contents_value_pclaim"
+    # "building_covg_pclaim", "contents_covg_pclaim",
+    # "damage_building_pvalue", "damage_contents_pvalue",
+    #"net_building_pmts_pdamage"
+)
 s_pclaim <- paste0("c(", paste0(v_pclaim, collapse = ", "), ")")
 
 v_out <- c("claims_n")
 s_out <- paste0("c(", paste0(v_out, collapse = ", "), ")")
 
 fmla_pclaim <- as.formula(paste0(
-    s_pclaim, " ~ i(year_constr, mh, ref = 1994) | countyfp^year_loss + mh"))
+    s_pclaim, " ~ i(year_constr, treated_mh, ref = 1994) + i(year_constr, mh, ref = 1994) | countyfp^year_loss + mh"))
 fmla_out    <- as.formula(paste0(
-    s_out, " ~ i(year_constr, mh, ref = 1994) | countyfp^year_loss + mh^year_loss"))
+    s_out, " ~ i(year_constr, treated_mh, ref = 1994) | countyfp^year_loss + mh^year_loss"))
 
 est_pclaim <- feols(
     fmla_pclaim, weights = ~claims_n,
@@ -57,7 +60,7 @@ etable(est_pois)
 est_ddd <- feols(
     c(building_damage_pclaim, net_building_pmt_pclaim) ~
         post1994 * mh * treated | countyfp^year_loss + mh,
-    data = dt_claims, cluster = ~countyfp
+    data = dt_claims
 )
 
-etable(est_ddd, se = "cluster")
+etable(est_ddd)
